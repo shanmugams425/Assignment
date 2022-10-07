12500c1a7a926475285a22bf0ea55a89db7b2a0f
shanmugam R <shanmugams425@gmail.com>
2022-10-08 01:41:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginReg from "./pages/auth/LoginReg";
+import ResetPassword from "./pages/auth/ResetPassword";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import { useSelector } from "react-redux";
+
+function App() {
+  const { token } = useSelector((state) => state.auth);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route
+              path="login"
+              element={!token ? <LoginReg /> : <Navigate to="/dashboard" />}
+            />
+
+            <Route
+              path="api/user/reset/:id/:token"
+              element={<ResetPassword />}
+            />
+          </Route>
+          <Route
+            path="/dashboard"
+            element={token ? <Dashboard /> : <Navigate to="/login" />}
+          />
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
